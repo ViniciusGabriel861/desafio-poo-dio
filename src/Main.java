12a449352ae095cbd3d0285078b83925eb0c2b71
ViniciusGabriel861 <vinicius.gabriel050604@gmail.com>
2023-11-08 21:53:01 -0300

    mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Lógica de programação");
+        curso1.setDescricao("legal");
+        curso1.setCargaHoraria(1);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("JAVA");
+        curso2.setDescricao("POO");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Explicações");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
